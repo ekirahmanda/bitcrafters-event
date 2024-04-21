1dd79d02217dc490e2dc984f1bf4c2677c552868
irinalalp <irinalaifa@gmail.com>
2024-04-21 12:51:22 +0800

    navbar without functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,50 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Home</a>
+                  <a>Login</a>
+                  <a>Register</a>
+                </li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost text-xl">BitEvents</a>
+          </div>
+          <div className="navbar-center hidden lg:flex"></div>
+          <div className="navbar-end">
+            <a className="btn">Log Out</a>
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
