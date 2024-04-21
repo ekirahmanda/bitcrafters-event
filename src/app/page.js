23e2ae44d89db3f157704111be12ac289dd5c848
Rizki Rahmanda <eki.rahmanda@gmail.com>
2024-04-21 13:43:11 +0700

    improve staging gas

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,10 @@ export default function Home() {
   return (
     <main>
       <Header />
-<<<<<<< HEAD
-      Ini adalah staging
-=======
-      Halo, Kak.
+      Halo, ini staging
       <div>
         <button className="btn btn-primary">Submit</button>
       </div>
->>>>>>> main
     </main>
   );
 }
