88f706875d6b4551171376be3bfc0a32abe9de67
bagasanin000 <bagasanindhito@gmail.com>
2024-04-21 21:32:25 +0700

    user dashboard page

---
+++ src/app/userdashboard/page.js
@@ -0,0 +1,41 @@
+import { UserDashboard } from "@/components/UserDashboard";
+import Cookies from "js-cookie";
+
+export function getUsers() {
+  const token = Cookies.get("token");
+  let userData = null;
+  const userCookie = Cookies.get("userData");
+  return { token, userData };
+}
+
+export async function listEvents() {
+  const res = await fetch("https://eventmakers.devscale.id/events", {
+    cache: "no-cache",
+  });
+  const data = await res.json();
+  return data;
+}
+export default async function Dashboard() {
+  const { userData } = getUsers();
+
+  const { data } = await listEvents();
+  const authoredEvent = data.filter(
+    (item) => item.events.author === userData.id
+  );
+  const joinedEvent = data.filter((item) =>
+    item.participants.some(
+      (participant) => participant.email === userData.email
+    )
+  );
+
+  console.log(userData);
+  console.log(data);
+  console.log(joinedEvent);
+  console.log(authoredEvent);
+
+  return (
+    <main>
+      <UserDashboard />
+    </main>
+  );
+}
