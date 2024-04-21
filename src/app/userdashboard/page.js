b35d38908ffbf5529acfe59604306f2909931633
Indra Zulfi <me@indrazm.com>
2024-04-21 22:27:12 +0700

    debugging user dashboard

--- src/app/userdashboard/page.js
+++ src/app/userdashboard/page.js
@@ -1,12 +1,12 @@
 import { UserDashboard } from "@/components/UserDashboard";
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
 
-export function getUsers() {
-  const token = Cookies.get("token");
-  let userData = null;
-  const userCookie = Cookies.get("userData");
-  return { token, userData };
-}
+// export function getUsers() {
+//   const token = Cookies.get("token");
+//   let userData = null;
+//   const userCookie = Cookies.get("userData");
+//   return { token, userData };
+// }
 
 export async function listEvents() {
   const res = await fetch("https://eventmakers.devscale.id/events", {
@@ -16,26 +16,19 @@ export async function listEvents() {
   return data;
 }
 export default async function Dashboard() {
-  const { userData } = getUsers();
+  // const { userData } = getUsers();
 
   const { data } = await listEvents();
-  const authoredEvent = data.filter(
-    (item) => item.events.author === userData.id
-  );
-  const joinedEvent = data.filter((item) =>
-    item.participants.some(
-      (participant) => participant.email === userData.email
-    )
-  );
-
-  console.log(userData);
   console.log(data);
-  console.log(joinedEvent);
-  console.log(authoredEvent);
+  // const authoredEvent = data.filter((item) => item.events.author === userData.id);
+  // const joinedEvent = data.filter((item) => item.participants.some((participant) => participant.email === userData.email));
 
-  return (
-    <main>
-      <UserDashboard />
-    </main>
-  );
+  // console.log(userData);
+  // console.log(data);
+  // console.log(joinedEvent);
+  // console.log(authoredEvent);
+
+  return <div>Dashboard</div>;
+
+  // return <UserDashboard />;
 }
