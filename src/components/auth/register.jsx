86efba12505099727ecbcf13707a39e1daed4f6d
irinalalp <irinalaifa@gmail.com>
2024-04-22 18:08:05 +0800

    register UI fixing-similiarize with login

--- src/components/auth/register.jsx
+++ src/components/auth/register.jsx
@@ -28,17 +28,43 @@ export const Register = () => {
     }
   }
   return (
-    <main>
-      <section>
-        <h1>Register</h1>
-      </section>
-      <form className="space-y-2" action={handleRegister}>
-        <input name="name" placeholder="Name" />
-        <input name="email" placeholder="Email" />
-        <input type="password" name="password" placeholder="Password" />
-        <button>Register</button>
-      </form>
-      {message !== "" ? <div>{message}</div> : null}
+    <main className="h-screen grid grid-cols-2 border border-cyan-500">
+      <div className="bg-cyan-500 flex justify-center items-center ">
+        <img
+          src="https://assets-prd.ignimgs.com/2021/09/16/bitcraft-button-1631814716884.jpg?width=300&crop=1%3A1%2Csmart&auto=webp&dpr=2"
+          width={500}
+          height={500}
+          alt="Bitcraft Logo from Internet"
+        />
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="w-[240px] space-y-3">
+          <h1 className="text-2xl">Login</h1>
+          <p className="text-lg">let's register, buddy!</p>
+          <form className="form-control gap-2" action={handleRegister}>
+            <input
+              className="input input-primary"
+              name="name"
+              placeholder="Enter your Name"
+            />
+            <input
+              className="input input-primary"
+              name="email"
+              placeholder="Enter your email"
+            />
+            <input
+              className="input input-primary"
+              name="password"
+              placeholder="Enter your password"
+              type="password"
+            />
+            <button className="btn btn-primary">Register</button>
+          </form>
+          <div className="text-sm">
+            {message !== "" ? <div>{message}</div> : null}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
